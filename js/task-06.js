1992837ceaeb992924b12f686a649d16fda5226e
Sychov-Yurii <sychov.yurii@outlook.com>
2023-06-18 11:40:35 +0200

    17

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,13 @@
-const inputWindowEl = document.querySelector('#validation-input')
-inputWindowEl.addEventListener()
+const inputEl = document.querySelector('#validation-input')
+inputEl.addEventListener()
 
+function borderColor() {
+    const inputValidEl = Number(inputEl.dataset.length);
+    const inputInvalidEl = inputEl.value.length;
+    inputEl.classList.remove("invalid", "valid");
+    if (inputInvalidEl === inputValidEl) {
+        inputEl.classList.add("valid");
+    } else {
+        inputEl.classList.add("invalid"); 
+    }
+}
