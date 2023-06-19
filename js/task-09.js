d454ba313b4b01ca5649ff88cee80c5ce6f2f705
Sychov-Yurii <sychov.yurii@outlook.com>
2023-06-19 20:42:57 +0200

    19

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorEl = document.querySelector(".color");
+const changeColorEl = document.querySelector(".change-color");
+const bodyEl = document.body;
+
+changeColorEl.addEventListener("click", onBtnClick);
+
+function onBtnClick () {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorEl.textContent = randomColor;
+}
+
+
