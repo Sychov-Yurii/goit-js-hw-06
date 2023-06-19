244966fece774ddb1ee6d29790b1806d106ca46e
Sychov-Yurii <sychov.yurii@outlook.com>
2023-06-19 11:04:47 +0200

    19

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,14 @@
-const inputWindowEl = document.querySelector('#validation-input')
-inputWindowEl.addEventListener()
-
+const inputEl = document.querySelector("#validation-input");
+inputEl.addEventListener("blur", borderColor);
+inputEl.classList.remove("invalid", 'valid')
 function borderColor() {
     const inputValidEl = Number(inputEl.dataset.length);
     const inputInvalidEl = inputEl.value.length;
-    inputEl.classList.remove("invalid", "valid");
+
     if (inputInvalidEl === inputValidEl) {
         inputEl.classList.add("valid");
     } else {
-        inputEl.classList.add("invalid"); 
+        inputEl.classList.add("invalid");
     }
+ 
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formLog = document.querySelector(".login-form");
+console.log(formLog);
 
+formLog.addEventListener("submit", hendlerFormLog);
+
+function hendlerFormLog(evt) {
+    evt.preventDefault();
+
+    console.log(evt.currentTarget.elements);
+
+    const { email, password } = evt.currentTarget.elements;
+
+    if (!email.value || !password.value) {
+        return alert `Всі поля повинні бути заповнені`;
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log("data", data);
+    console.log(Object.keys(evt.currentTarget.elements));
+    evt.currentTarget.reset();
+}
