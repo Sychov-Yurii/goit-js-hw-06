0fa72947a2276caac38fbef9d92a14cc48de4880
Sychov-Yurii <sychov.yurii@outlook.com>
2023-06-22 08:57:53 +0200

    21

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
+  'Potatoes', // 0
+  'Mushrooms', // 1
+  'Garlic', // 2
+  'Tomatos', 
   'Herbs',
   'Condiments',
 ];
 
 const list = document.querySelector('ul#ingredients');
+const arr = [];
 
-ingredients.forEach(ingredient => {
-  const listItem = document.createElement('li');
-  listItem.textContent = ingredient;
-  listItem.classList.add('item');
-  list.appendChild(listItem);
+
+ingredients.forEach((ingredient, index, array) => {
+  arr.push(ingredient)
+  console.log(arr);
+  // const listItem = document.createElement('li');
+  // listItem.textContent = ingredient;
+  // listItem.classList.add('item');
+  
 });
+
+//list.appendChild(listItem);
