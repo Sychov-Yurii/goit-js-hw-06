0ca303970e257a8ced26f08d6188a7b7b6dc5393
Sychov-Yurii <sychov.yurii@outlook.com>
2023-09-19 17:24:38 +0200

    01

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,15 @@
 const categoriesList = document.querySelector('ul#categories');
 const items = categoriesList.querySelectorAll('li.item');
 
+console.log("Number of categories: ", items.length);
 items.forEach(item => {
   
   const categoryTitle = item.querySelector('h2').textContent;
   const categoryElements = item.querySelectorAll('li');
-  
+
+  console.log("Category: ", item.children[0].textContent);
+  console.log("Elements: ", item.children[1].children.length);
+
 });
+
+
