8b4530fab6fe5b5ccb48af670c2b3f5d570798cc
Sychov-Yurii <sychov.yurii@outlook.com>
2023-09-20 09:35:22 +0200

    20

--- js/task-02.js
+++ js/task-02.js
@@ -23,5 +23,28 @@ const markup = ingredients.map((elem) => {
   console.log(elem);
 
 
+//   // Отримуємо посилання на список ul#ingredients
+// const ul = document.getElementById('ingredients');
+
+// // Отримуємо масив інгредієнтів
+// const ingredients = ['Інгредієнт 1', 'Інгредієнт 2', 'Інгредієнт 3'];
+
+// // Цикл для обробки кожного інгредієнта
+// ingredients.forEach(ingredient => {
+//   // Створюємо новий елемент <li>
+//   const li = document.createElement('li');
+  
+//   // Додаємо текстовий вміст з назвою інгредієнта
+//   li.textContent = ingredient;
+  
+//   // Додаємо клас "item" до елементу <li>
+//   li.classList.add('item');
+  
+//   // Вставляємо елемент <li> у список ul#ingredients
+//   ul.appendChild(li);
+// });
+
+
+
 
 
