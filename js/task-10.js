06dd79de488b8d9fe6737ed37dbcb1e62f0b208a
Sychov-Yurii <sychov.yurii@outlook.com>
2023-09-24 21:56:25 +0200

    10 hw 50%

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+  const numberEl = document.querySelector("input");
+  const btnCreate = document.querySelector("[data-create]");
+  const btnDestroy = document.querySelector("[data-destroy]");
+  const boxEl = document.querySelector("#boxes");
+  const primaryValueSize = 30;
+  btnCreate.addEventListener("click", onBtnCreateClick);
+  btnDestroy.addEventListener("click", onBtnDestroyClick);
+
+  function onBtnCreateClick() {
+    createBoxes(numberEl.value);
+  }
+  function createBoxes(amount) {
+    const newArray = [];
+    for (let i = 0; i < amount; i += 1) {
+        newArray[i] = i;
+    }
+  }
+
+
+
+
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215)
 //     .toString(16)
